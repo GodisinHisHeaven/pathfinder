1009093ba744e9e7896eb5df1115777ccc45ecae
mingjunliu <mingjun6@illinois.edu>
2022-03-28 22:31:30 -0500

    20220328 updata development log, Makefile, BFS

---
+++ BFS.h
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @file BFS.h
+ * @authors Mingjun Liu (mingjun6) 
+ * @brief This file contains the BFS class.
+ * @date 2022-03-28
+ * 
+ */
+
+#pragma once
+
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class BFS
+{
+private:
+    /* data */
+public:
+    BFS(/* args */);
+    ~BFS();
+};
+
+
+
+
+
