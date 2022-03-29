627a223245765f7ed1524e61dda77ebdab73c376
mingjunliu <mingjun6@illinois.edu>
2022-03-29 13:32:13 -0500

    20220329 update BFS

--- BFS.h
+++ BFS.h
@@ -1,7 +1,10 @@
 /**
  * 
  * @file BFS.h
- * @authors Mingjun Liu (mingjun6) 
+ * @author Mingjun Liu (mingjun6)
+ * @author
+ * @author
+ * @author
  * @brief This file contains the BFS class.
  * @date 2022-03-28
  * 
