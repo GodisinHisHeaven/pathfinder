87969344febffbaedb0a02faed77cb0dcd37233f
Winnie5683 <nieyiheng2001@me.com>
2022-05-05 12:26:32 -0500

    update BFS testcase

--- tests/test_BFS.cpp
+++ tests/test_BFS.cpp
@@ -2,3 +2,20 @@
 #include "../includes/Graph.h"
 #include "../includes/GraphReader.h"
 #include <iostream>
+
+TEST_CASE("BFS works on small dataset", "[BFS]") {
+   Graph graph;
+    std::string nodeFilePath = "tests/test.cnode";
+    std::string edgeFilePath = "tests/test.cedge";
+    GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
+    graphReader.readNodes();
+    graphReader.readEdges();
+
+   std::vector<int> path = graph.BFS(0, 4); // throws exception
+
+    REQUIRE(path.size() == 3);
+    REQUIRE(path[1] == 2);
+    REQUIRE(path[2] == 3);
+    REQUIRE(path[3] == 4);
+   
+}
