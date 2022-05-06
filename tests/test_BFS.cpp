83fcb566e8aabfba7ee184e17080388562b07b15
Winnie5683 <nieyiheng2001@me.com>
2022-05-05 21:52:05 -0500

    update BFS test

--- tests/test_BFS.cpp
+++ tests/test_BFS.cpp
@@ -3,20 +3,107 @@
 #include "../includes/GraphReader.h"
 #include <iostream>
 
-TEST_CASE("BFS works on small dataset", "[BFS]") {
+/**
+ *        2 — — 4      7
+ *      /     / |  \   |
+ *    1         |   6  |
+ *      \   /   |  /   |
+ *    0   3 — — 5      8
+ * 
+ * a brief graph of BFS.cnode and BFS.cedge
+**/
+
+TEST_CASE("BFS works on simple route", "[line]") {
     Graph graph;
-    std::string nodeFilePath = "tests/test.cnode";
-    std::string edgeFilePath = "tests/test.cedge";
+    std::string nodeFilePath = "tests/BFS.cnode";
+    std::string edgeFilePath = "tests/BFS.cedge";
     GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
     graphReader.readNodes();
     graphReader.readEdges();
 
-    std::vector<int> path;
+    std::vector<int> path = graph.BFS(7,8);
 
-    path = graph.BFS(0,4);
+    REQUIRE(path.size() == 2);
+    REQUIRE(path[0] == 8);
+    REQUIRE(path[1] == 8);
+
+
+}
+
+TEST_CASE("BFS works on cyclic route 1", "[cycle]") {
+    Graph graph;
+    std::string nodeFilePath = "tests/BFS.cnode";
+    std::string edgeFilePath = "tests/BFS.cedge";
+    GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
+    graphReader.readNodes();
+    graphReader.readEdges();
+
+    std::vector<int> path = graph.BFS(1,5);
 
     REQUIRE(path.size() == 5);
-    REQUIRE(path[1] == 2);
-    REQUIRE(path[2] == 3);
-    REQUIRE(path[3] == 4);
+    REQUIRE(path[0] == 2);
+    REQUIRE(path[1] == 3);
+    REQUIRE(path[2] == 4);
+    REQUIRE(path[3] == 5);
+    REQUIRE(path[4] == 5);
+
 }
+
+TEST_CASE("BFS works on cyclic route 2", "[cycle]") {
+    Graph graph;
+    std::string nodeFilePath = "tests/BFS.cnode";
+    std::string edgeFilePath = "tests/BFS.cedge";
+    GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
+    graphReader.readNodes();
+    graphReader.readEdges();
+
+    std::vector<int> path = graph.BFS(2,6);
+
+    REQUIRE(path.size() == 4);
+    REQUIRE(path[0] == 4);
+    REQUIRE(path[1] == 5);
+    REQUIRE(path[2] == 6);
+    REQUIRE(path[3] == 6);
+
+}
+
+TEST_CASE("BFS works on cyclic route 3", "[cycle]") {
+    Graph graph;
+    std::string nodeFilePath = "tests/BFS.cnode";
+    std::string edgeFilePath = "tests/BFS.cedge";
+    GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
+    graphReader.readNodes();
+    graphReader.readEdges();
+
+    std::vector<int> path = graph.BFS(1,6);
+
+    REQUIRE(path.size() == 6);
+    REQUIRE(path[0] == 2);
+    REQUIRE(path[1] == 3);
+    REQUIRE(path[2] == 4);
+    REQUIRE(path[3] == 5);
+    REQUIRE(path[4] == 6);
+    REQUIRE(path[5] == 6);
+
+}
+
+TEST_CASE("BFS works on dsiconnected route ", "[disconnected]") {
+    Graph graph;
+    std::string nodeFilePath = "tests/BFS.cnode";
+    std::string edgeFilePath = "tests/BFS.cedge";
+    GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
+    graphReader.readNodes();
+    graphReader.readEdges();
+
+    std::vector<int> path = graph.BFS(1,7);
+
+    REQUIRE(path.size() == 6);
+    REQUIRE(path[0] == 2);
+    REQUIRE(path[1] == 3);
+    REQUIRE(path[2] == 4);
+    REQUIRE(path[3] == 5);
+    REQUIRE(path[4] == 6);
+    REQUIRE(path[5] != 6);
+
+}
+
