c9c9434d5eff58436458b1006ac11eb8e5141f0f
Winnie5683 <nieyiheng2001@me.com>
2022-05-06 16:52:09 -0500

    update data parsing test

--- tests/test_data_parsing.cpp
+++ tests/test_data_parsing.cpp
@@ -59,30 +59,28 @@ TEST_CASE("GraphReader reads nodes correctly", "[Data Parsing]") {
     REQUIRE(statement == true);
 }
 
-//TEST_CASE("GraphReader reads edges correctly", "[Data Parsing]") {
-//    Graph graph;
-//    std::string nodeFilePath = "dataset/NA.cnode";
-//    std::string edgeFilePath = "dataset/NA.cedge";
-//    GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
-//    graphReader.readNodes();
-//    graphReader.readEdges();
-//
-//    std::ifstream edgesFile(edgeFilePath);
-//    int ID;
-//    int start, end;
-//    double dist;
-//    bool statement = true;
-//    while (edgesFile >> ID >> start >> end >> dist) {
-//
-//        for (size_t i = 0; i < graph.getAdjList().at(start).size(); i++) {
-//            if (graph.getAdjList().at(start).at(i).ID == end && graph.getAdjList().at(start).at(i).dist != dist) {
-//                statement = false;
-//                std::cout << "wrong data at " << ID << std::endl;
-//                break;
-//            }
-//        }
-//
-//    }
-//
-//    REQUIRE(statement == true);
-//}
+TEST_CASE("GraphReader reads edges correctly", "[Data Parsing]") {
+   Graph graph;
+   std::string nodeFilePath = "dataset/NA.cnode";
+   std::string edgeFilePath = "dataset/NA.cedge";
+   GraphReader graphReader(&graph, nodeFilePath, edgeFilePath);
+   graphReader.readNodes();
+   graphReader.readEdges();
+
+   std::ifstream edgesFile(edgeFilePath);
+   int ID;
+   int start, end;
+   double dist;
+   bool statement = true;
+   while (edgesFile >> ID >> start >> end >> dist) {
+
+    if (graph.getAdjList().at(start).at(end).ID == end && graph.getAdjList().at(start).at(end).dist != dist) {
+           statement = false;
+           std::cout << "wrong data at " << ID << std::endl;
+           break;
+    }
+
+   }
+
+   REQUIRE(statement == true);
+}
